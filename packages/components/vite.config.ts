4166366a38994686346270c280d0b4220af63274
Abel van Beek <abel.van.beek@tromsfylke.no>
2024-07-18 15:04:16 +0200

    feat: wrapped Shoelace (working with Vite plugin transforming  to

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite';
+import dts from "vite-plugin-dts"
 import { resolve } from 'path';
+import { dependencies } from './package.json'
 
 export default defineConfig({
+  plugins: [dts({
+    include: ['./src/**/*']
+  })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'components',
     },
+    rollupOptions: {
+      external: Object.keys(dependencies),
+      output: {
+        globals: {
+          "@shoelace-style/localize": "@shoelace-style/localize",
+          "@shoelace-style/shoelace": "@shoelace-style/shoelace",
+          "@tfk-samf/tokens": "@tfk-samf/tokens"
+        }
+      }
+    }
   },
 });
