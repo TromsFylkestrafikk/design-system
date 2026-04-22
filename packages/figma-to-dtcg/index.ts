e633b84da0e3b3327c79b7bf3b281839eb1f9489
Håkon Strandlie <hakon.strandlie@atb.no>
2026-04-22 10:50:56 +0200

    refactor: Removed unnecessary capture group in regex, and added Success

--- packages/figma-to-dtcg/index.ts
+++ packages/figma-to-dtcg/index.ts
@@ -177,7 +177,7 @@ async function valueToJSON(
 
   const isForeground = (
     { name: _name }: LocalVariable,
-  ): boolean => !!_name.match(/^Foreground\/([\w\s]+(?:\/[\w\s]+)?)\/(Primary|Secondary|Disabled|Info|Error|Interactive)$/);
+  ): boolean => !!_name.match(/^Foreground\/([\w\s]+)\/(Primary|Secondary|Disabled|Info|Error|Interactive|Success)$/);
 
   const isColorPalette = (
     { name: _name }: LocalVariable,
